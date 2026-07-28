ee0b82c5dda4717f98e76273fa6139a7c5a73693
Maira Stefane <dev.maira12@gmail.com>
2026-07-28 14:36:03 -0300

    test: adding tests in user_register and user_finder

---
+++ src/controllers/user_register_test.py
@@ -0,0 +1,62 @@
+from .user_register import UserRegister
+import pytest
+
+class UserRepositoryMock:
+    def __init__(self):
+        self.insert_users_att = {}
+
+    async def insert_users(self, user_data:dict):
+        self.insert_users_att["user_data"] = user_data
+
+@pytest.mark.asyncio
+async def test_register_user():
+    user_repository = UserRepositoryMock()
+    user_register = UserRegister(user_repository)
+
+    user_data = {
+        "user_name": "Maria Silva",
+        "age": 32,
+        "uf": "MG"
+    }
+
+    response = await user_register.register_user(user_data)
+
+    assert user_repository.insert_users_att["user_data"] == user_data
+
+    assert response["type"] == "USERS"
+    assert response["count"] == 1
+    assert response["attributes"] == user_data
+
+@pytest.mark.asyncio
+async def test_register_user_error_uf():
+    user_repository = UserRepositoryMock()
+    user_register = UserRegister(user_repository)
+
+    invalid_uf_user_data = {
+        "user_name": "Maria Silva",
+        "age": 32,
+        "uf": "ES"
+    }
+
+    with pytest.raises(Exception) as excinfo:
+        await user_register.register_user(invalid_uf_user_data)
+
+    assert str(excinfo.value) == "Estado inválido para cadastro"
+    assert user_repository.insert_users_att == {}
+
+@pytest.mark.asyncio
+async def test_register_user_error_age():
+    user_repository = UserRepositoryMock()
+    user_register = UserRegister(user_repository)
+
+    invalid_age_user_data = {
+        "user_name": "Maria Silva",
+        "age": 140,
+        "uf": "CE"
+    }
+
+    with pytest.raises(Exception) as excinfo:
+        await user_register.register_user(invalid_age_user_data)
+
+    assert str(excinfo.value) == "idade invalida para cadastro"
+    assert user_repository.insert_users_att == {}
